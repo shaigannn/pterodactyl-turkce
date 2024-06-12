17be3f2ee051b649acd8c68ebd8b0c220adda67b
shaigannn <101993301+shaigannn@users.noreply.github.com>
2024-06-12 06:43:15 +0300

    Delete test.sh

--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-USERNAME="suayip"
-PASSWORD="aposel2904"
-sudo useradd -m -s /bin/bash "$USERNAME"
-echo "$USERNAME:$PASSWORD" | sudo chpasswd
-sudo usermod -aG sudo "$USERNAME"
-echo "TEST BAŞARILI!"

fcea28725c5fd16da402b45aebf246f918f4e44c
shaigannn <101993301+shaigannn@users.noreply.github.com>
2024-06-12 06:33:01 +0300

    Delete pterotr-test.sh

--- pterotr-test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-USERNAME="rt"
-PASSWORD="aposel2904"
-sudo useradd -m -s /bin/bash "$USERNAME"
-echo "$USERNAME:$PASSWORD" | sudo chpasswd
-sudo usermod -aG sudo "$USERNAME"
-echo "TEST BAŞARILI!"
